745b177f4f95ae51a09f0effdf34cf0f166c7f5c
Diogo <Diogomeireeles@gmail.com>
2023-11-12 23:06:36 -0300

    first

---
+++ DiogoUdf/src/Minado/CampoMinado.java
@@ -0,0 +1,16 @@
+/* Aluno: Diogo Meireles De Almeida 
+  RGM: 24933961
+  IDE: Eclipse 
+  Linguagem: Java
+  */
+
+
+package Minado;
+
+public class CampoMinado {
+    public static void main(String[] args) {
+        Jogo jogo = new Jogo();
+
+    }
+
+}
